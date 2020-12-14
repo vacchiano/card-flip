3d749ab4046c9cc68016bb039ff5a7c72b62304b
Dom Vacchiano <djvacchiano@gmail.com>
2020-12-14 00:13:58 -0600

    updated ebay scraping and added navbar

--- getprice/views.py
+++ getprice/views.py
@@ -8,7 +8,8 @@ def webScrape(query):
     #url = "https://www.ebay.com/sch/i.html?_from=R40&_trksid=m570.l1313&_nkw=2007+Patrick+Kane+Upper+Deck+Young+Guns+Rookie+Card+mint&_sacat=0&LH_TitleDesc=0&_fsrp=1&LH_PrefLoc=3&_sop=13&_osacat=0&_odkw=2007+Patrick+Kane+Upper+Deck+Young+Guns+Rookie+Card+min&LH_Sold=1"
     #short_url = "https://www.ebay.com/sch/i.html?_from=R40&_trksid=m570.l1313&_nkw=Patrick+Kane+Young+Guns+Rookie+Card+Mint+2007&LH_Sold=1"
 
-    url = (f'https://www.ebay.com/sch/i.html?_from=R40&_trksid=m570.l1313&_nkw={query}&LH_Sold=1')
+    url = (f'https://www.ebay.com/sch/i.html?_from=R40&_trksid=m570.l1313&_nkw={query}+-lot&LH_Sold=1')
+    print(url)
 
     r = requests.get(url)
     soup = BeautifulSoup(r.text, 'html.parser')
@@ -22,34 +23,45 @@ def webScrape(query):
 
     else:    
         for listing in listings[0:5]:
-            #print(listing)
-            #title = soup.find(class_='s-item__title').get_text()
-            price = listing.find(class_='POSITIVE').get_text()
-            price = price.replace("$", "")
-            price = float(price)
+            try:
+                #print(listing)
+                #title = soup.find(class_='s-item__title').get_text()
+                price = listing.find(class_='s-item__price').get_text()
+                price = price.replace("$", "")
+                price = price.replace(",", "")
+                print(price)
+                price = float(price)
 
-            shipping = listing.find(class_='s-item__shipping').get_text()
-            if shipping == "Free shipping":
-                shipping = 0.00
-            else:
-                shipping = shipping.replace("+$", "")
-                shipping = shipping.replace(" shipping", "")
-                shipping = float(shipping)
-            
-            # to go from thumnail to large img remove 'thumbs/' and remplace '225' w/ '500
-            # https://i.ebayimg.com/thumbs/images/g/JzgAAOSwcwRextIc/s-l225.jpg
-            img = listing.find(class_='s-item__image-img')['src']
-            img_url = img.replace("thumbs/", "")
-            img_url = img_url.replace("225", "500")
-            
-            avg_price += price
-            avg_price += shipping
+                shipping = listing.find(class_='s-item__shipping').get_text()
+                if shipping == "Free shipping":
+                    shipping = 0.00
+                else:
+                    shipping = shipping.replace("+$", "")
+                    shipping = shipping.replace(" shipping", "")
+                    shipping = float(shipping)
+                
+                # to go from thumnail to large img remove 'thumbs/' and remplace '225' w/ '500
+                # https://i.ebayimg.com/thumbs/images/g/JzgAAOSwcwRextIc/s-l225.jpg
+                img = listing.find(class_='s-item__image-img')['src']
+                img_url = img.replace("thumbs/", "")
+                img_url = img_url.replace("225", "500")
+                
+                avg_price += price
+                avg_price += shipping
     
-    avg_price = float(avg_price/5)
-    avg_price = round(avg_price, 2)
-    avg_price = "{:.2f}".format(avg_price)
+                
+            except:
+                print('error')
+                avg_price = "0"
 
-    return [img_url, avg_price]
+        avg_price = float(avg_price/5)
+        avg_price = round(avg_price, 2)
+        avg_price = "{:.2f}".format(float(avg_price))
+        avg_price = "{:,}".format(float(avg_price))
+        #print(avg_price)
+
+        return [img_url, avg_price]
+
 
 
 # Create your views here.
@@ -71,10 +83,10 @@ def search(request):
         query += " " + request.POST['card_year']
         request.session['year'] = request.POST['card_year']
 
-        request.session['mint'] = request.POST['is_mint']
-        mint = request.POST['is_mint']
-        if (mint):
-            query += " " + "mint"
+        request.session['card_psa'] = request.POST.get('card_psa', '')
+        psa = request.POST.get('card_psa', '')
+        if (psa != ''):
+            query += " PSA" + psa
         
         query = query.replace("  ", " ")
         query = query.replace(" ", "+")
